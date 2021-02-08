38cabebcedfd90c1de5b8945335791925bffc726
todian-tornero <76249448+todian-tornero@users.noreply.github.com>
2021-02-08 11:38:24 +0300

    Пробуем создать веб-приложение

---
+++ src/main/java/Configs.java
@@ -0,0 +1,7 @@
+public class Configs {
+    protected String dbHost = "localhost";
+    protected String dbPort = "3306";
+    protected String dbUser = "root";
+    protected String dbPass = "TornerO410790%@";
+    protected String dbName = "ATM";
+}
---
+++ src/main/java/DBWork.java
@@ -0,0 +1,5 @@
+public class DBWork {
+    public static void main(String[] args) {
+
+    }
+}
